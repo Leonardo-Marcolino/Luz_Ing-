a1684821d38aa901765c436a69a0f97a110a7576
leonardo-marcolino <leo.marcolino14@gmail.com>
2023-02-17 17:11:34 -0300

    colocando interação no botão de comprar

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ $("nav a").click(function(event){
     }
     $('html,body').animate({scrollTop:dest}, 1000,'swing');
   });
+
+
+
+  function aviso(){
+
+    alert("Ainda não vendemos pelo site, porem ligue para nós informando  os itens desejaveis para que possamos montar um orçamento, para você. Telefone : (+55) 11 99999-9980")
+  }
